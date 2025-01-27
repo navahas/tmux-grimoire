bf8ad058b0434aacc87bc9ee3242e578230e4383
cnavajas <c.navajas4@gmail.com>
2025-01-27 17:16:41 +0100

    feat: buoyshell is now the window 1 for sessions

--- scripts/buoy.sh
+++ scripts/buoy.sh
@@ -23,8 +23,17 @@ if [[ $? -ne 0 ]]; then
     tmux set-option -t "$session_name" status off
 fi
 
-window_name="$current_session"
+window_name="buoyshell_$current_session"
 session_dir=$(tmux display-message -t "$current_session" -p '#{pane_current_path}')
+
+# Check if the current session has a window in position :1
+if tmux list-windows -t "$current_session" -F '#{window_index}' | grep -q '^1$'; then
+    # Rename the window in position :1 to buoy_<session_name>
+    tmux rename-window -t "$current_session:1" "$window_name"
+else
+    # Create a new window in position :1 for the current session
+    tmux new-window -d -t "$current_session:1" -n "$window_name" -c "$session_dir"
+fi
 
 # Check if shell for the current session already exists in the popup session
 if ! tmux list-windows -t "$session_name" -F '#{window_name}' | grep -q "^$window_name$"; then
@@ -34,6 +43,9 @@ fi
 # Ensure temp_window is cleaned
 tmux kill-window -t "$session_name:$temp_window" 2>/dev/null
 
+# Move the window from the current session to the manager session
+tmux swap-window -s "$current_session:1" -t "$session_name:$window_name"
+
 # Launch Popup
 tmux display-popup \
   -E \
@@ -42,3 +54,7 @@ tmux display-popup \
   -w"$width" -h"$height" \
   -T " session: $current_session " \
   "tmux attach-session -t '$session_name' \; select-window -t '$current_session'"
+
+
+# Move the window back to the original session when the popup is detached
+tmux set-hook -g -t "$session_name" client-detached "run-shell 'tmux swap-window -s \"$session_name:$window_name\" -t \"$current_session:1\"'"
